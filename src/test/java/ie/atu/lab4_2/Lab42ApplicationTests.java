929a43b5499a4c584c9a1f2d55191fc5ec58e49b
Aleks <G00396018@atu.ie>
2023-10-11 09:40:37 +0100

    Setup Project

---
+++ src/test/java/ie/atu/lab4_2/Lab42ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.lab4_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab42ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
